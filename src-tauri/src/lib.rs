b94ab4b8ce3633d760ac7bd7422dadfcfd7b7772
Mario <6395323+mario-huang@users.noreply.github.com>
2025-01-19 13:48:34 +0800

    add react

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,14 @@
-use std::process::Command;
-use tauri::{path::BaseDirectory, Manager};
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
-            let install_script = app
-                .path()
-                .resolve("Whisper-WebUI/Install.sh", BaseDirectory::Resource);
-            Command::new("sh")
-                .arg(install_script.unwrap())
-                .status()
-                .expect("failed to run Install.sh");
-            Ok(())
-        })
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
