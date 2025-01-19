557d46259032fa459d2e5ac7baa837fd3c3dc088
Mario <6395323+mario-huang@users.noreply.github.com>
2025-01-19 13:08:08 +0800

    try call from rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,19 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use std::process::Command;
+use tauri::{path::BaseDirectory, Manager};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let install_script = app
+                .path()
+                .resolve("Whisper-WebUI/Install.sh", BaseDirectory::Resource);
+            Command::new("sh")
+                .arg(install_script.unwrap())
+                .status()
+                .expect("failed to run Install.sh");
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
