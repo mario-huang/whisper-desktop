b24ddd008f6158a4e909a41b13134cf7f496b479
Mario <6395323+mario-huang@users.noreply.github.com>
2025-01-20 19:57:07 +0800

    add startup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { LoaderPinwheel } from "lucide-react";
 import "./App.css";
 import { useWhisper } from "./whisper";
 
 function App() {
-
   useWhisper();
 
   return (
-    <>
-      
-    </>
+    <div className="flex flex-col gap-8 justify-center items-center h-screen text-orange-200  bg-orange-600">
+      <LoaderPinwheel className="size-40 animate-spin" />
+      <p className="text-2xl">
+        Whisper will complete startup in a few seconds.
+      </p>
+    </div>
   );
 }
 
