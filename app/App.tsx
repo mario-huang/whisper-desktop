0f1bdb0814417e982eb64db589d00158e779b914
Mario <6395323+mario-huang@users.noreply.github.com>
2025-01-27 12:20:39 +0800

    add tailwindcss and shadcn

--- app/App.tsx
+++ app/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { LoaderPinwheel } from "lucide-react";
+import "./App.css";
+import { useWhisper } from "./whisper";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const info = useWhisper();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col gap-8 justify-center items-center h-screen text-orange-200  bg-orange-600">
+      <LoaderPinwheel className="size-40 animate-spin" />
+      {/* <p className="text-2xl whitespace-pre text-center">{info}</p> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
